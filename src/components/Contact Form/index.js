e92a0bfdb1f18e95181a789aea0978bf5e50cfeb
kesiahp18 <kesiahp18@gmail.com>
2021-03-30 23:02:31 -0400

    contact form

---
+++ src/components/Contact Form/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import validateEmail from './emailValidation'
+
+const ContactForm = () => {
+    const [formInputs, setFormInputs] = useState({ name: '', email: '', message: '' })
+    const [errorMessage, setErrorMessage] = useState() 
+
+    const {name, email, message} = formInputs
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // do something with the backend that isnt there yet
+        if (errorMessage) {
+            console.log('fix errors before submitting')
+        } else {
+            console.log(formInputs)
+        }
+
+        setFormInputs({ name: '', email: '', message: '' })
+    }
+    const handleChange = (e) => {
+        if (e.target.value.length < 1) {
+            e.target.classList.add('error-class')
+            setErrorMessage(`Please enter a${e.target.name === 'email' ? 'n' : ''} ${e.target.name} before submitting!`)
+        } else if (e.target.name === 'email' && !validateEmail(e.target.value)) {
+            e.target.classList.add('error-class')
+            setErrorMessage("Invalid email!")
+        } else {
+            e.target.classList.remove('error-class')
+            setErrorMessage()
+            setFormInputs({...formInputs, [e.target.name]: e.target.value})
+        }
+    }
+
+    return (
+        <div>
+            <p><i>This form is for grade requirement and is for display only</i></p>
+            <form className="contact-form" onSubmit={handleSubmit}>
+                <div className="form-container">
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label" htmlFor="name">Name:</label>
+                        <div className="col-sm-10">
+                            <input className="form-control" type="text" name="name" defaultValue={name} onBlur={handleChange} />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label" htmlFor="email">Email:</label>
+                        <div className="col-sm-10">
+                            <input className="form-control" type="email" name="email" defaultValue={email} onBlur={handleChange} />                            
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label" htmlFor="message">Message:</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" name="message" rows="5" defaultValue={message} onBlur={handleChange} />                            
+                        </div>
+                    </div>
+                </div>
+                {
+                    errorMessage && (
+                        <div className="error-message-div">
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )
+                }
+                <div className="form-submit">
+                    <button type="submit" className="btn btn-dark">Submit</button>
+                </div>
+                
+            </form>
+        </div>
+    )
+}
+
+export default ContactForm
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ContactForm from './Contact Form/index'
 
 function Contact() {
     return (
-        <div>
+        <div className="main-info">
             <h1 className="title">CONTACT ME</h1>
             <p>View My <a href="https://github.com/kesiahp18">GitHub</a></p>
+            <ContactForm />
         </div>
     )
 }
